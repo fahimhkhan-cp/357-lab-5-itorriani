de685f85e7fae296a82d31c4c19d9d2ca7162da2
itorriani <itorrian@calpoly.edu>
2026-01-28 20:32:26 -0800

    feat * added logic for both

--- task2/array_list.c
+++ task2/array_list.c
@@ -16,4 +16,33 @@ array_list *array_list_new( size_t capacity) // include arguments for location a
     return newList; //return the completed object
 }
 
+void array_list_add_to_end(char *newString, array_list *list) // function signature with input for newString to be put at end
+{
+    size_t currLength = list->length; //get the list's current length
+
+    size_t capacity = list->capacity; // save the list's capacity
+
+    char* endPointer = (list->arr) + (currLength); // start a pointer and increment to the end of the list
+
+    size_t newStringSize = sizeof(newString); // get the size of the newString in bytes
+
+    if ( ((currLength + newStringSize)) > capacity) // check if we were going to extend the capacity
+    {
+        printf("Capacity Exceeded");
+    }
+
+    char* newStringCurr = newString; // start a pointer to newString
+
+    while (*newString != '\0') // iterate until the newString is over
+    {
+        *endPointer = *newString; // write the newString value to the end of the array
+
+        newString++; // increment the newString
+
+        endPointer++; // increment the endPointer;
+    }
+
+    return;
+}
+
 #endif
