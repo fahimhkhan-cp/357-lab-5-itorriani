35df0484e13d10c503b0cb8449f5f813acd4adb0
itorriani <itorrian@calpoly.edu>
2026-01-29 09:55:28 -0800

    feat * finished reallocation logic

--- task2/array_list.c
+++ task2/array_list.c
@@ -16,31 +16,32 @@ array_list *array_list_new( size_t capacity) // include arguments for location a
     return newList; //return the completed object
 }
 
-void array_list_add_to_end(char *newString, array_list *list) // function signature with input for newString to be put at end
+void array_list_add_to_end(array_list *list, char *newString) // function signature with input for newString to be put at end
 {
     size_t currLength = list->length; //get the list's current length
 
     size_t capacity = list->capacity; // save the list's capacity
 
-    char* endPointer = (list->arr) + (currLength); // start a pointer and increment to the end of the list
+   
 
     size_t newStringSize = sizeof(newString); // get the size of the newString in bytes
 
-    if ( ((currLength + newStringSize)) > capacity) // check if we were going to extend the capacity
+    if ( currLength == capacity) // check if the list is at capacity already. 
     {
-        printf("Capacity Exceeded");
+        size_t newCapacity = list->length + 1; //set a new capacity length
+        
+        list->arr = realloc(list->arr, newCapacity * sizeof(*list->arr)); // reallocate in bytes
+
+        list->capacity = newCapacity; //update the new capacity
     }
+
+    char** endPointer = (list->arr) + (currLength); // start a pointer and increment to the end of the list
 
     char* newStringCurr = newString; // start a pointer to newString
 
-    while (*newString != '\0') // iterate until the newString is over
-    {
-        *endPointer = *newString; // write the newString value to the end of the array
+    *endPointer = newString; // append the new string to the end of the array
 
-        newString++; // increment the newString
-
-        endPointer++; // increment the endPointer;
-    }
+    list->length++; // keep track of the new lenght
 
     return;
 }
